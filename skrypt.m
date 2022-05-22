78456bb38c24fbd2a9b63e6a414531476d089d9f
KonstantyOdwazny <61802175+KonstantyOdwazny@users.noreply.github.com>
2022-05-22 16:20:35 +0200

    Badanie wariancji i funkcji autokorelacji sygnału

--- skrypt.m
+++ skrypt.m
@@ -3,11 +3,23 @@ Data = load('cstr.dat');
 t = Data(:,1);
 u = Data(:,2);
 y = Data(:,4);
-plot(t,u,t,y);
-legend('u','y')
+% plot(t,u,t,y);
+% legend('u','y')
+% figure()
+% plot(t,u)
+% title('u')
+% figure()
+% plot(t,y)
+% title('y')
+N = 7500;
+Tp = 0.1;
+n = -N+1:1:N-1;
+sigmau = var(u);
+avgu = mean(u);
+sigmay = var(y);
+avgy = mean(y);
+[ruu, tauu] = xcorr(u','biased');
+[ryy, tauy] = xcorr(y,'biased');
+
 figure()
-plot(t,u)
-title('u')
-figure()
-plot(t,y)
-title('y')
+plot(n*Tp, ryy)
